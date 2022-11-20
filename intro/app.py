2ebd8d111e36e313636958e0f7081b35ef0fab2c
Thiago Eiji Matumoto <thiagoeijimatumoto@gmail.com>
2022-11-20 15:59:38 -0300

    Adiciando template e o bootstrap junto com novas rotas

--- intro/app.py
+++ intro/app.py
@@ -1,22 +1,39 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for, render_template
+from flask_bootstrap import Bootstrap5
 
 app = Flask(__name__)
+bootstrap = Bootstrap5(app)
 
 @app.route("/")
 def index():
     return "<a href='/posts'>Posts</a>"
 
+@app.route("/redirect")
+def redirect_method():
+    return redirect(url_for("response"))
+
+@app.route("/response")
+def response():
+    return render_template("response.html")
+
 @app.route("/posts")
-def posts():
-    data = dict(path=request.path,
+@app.route("/posts/<int:id>")
+def posts(id):
+    titulo = request.args.get("titulo")
+    
+    data = dict(
+            path=request.path,
             referrer=request.referrer,
             content_type=request.content_type,
-            method=request.method)
+            method=request.method,
+            titulo=titulo,
+            id=id if id else 0)
+    
     return data
 
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
 
 
